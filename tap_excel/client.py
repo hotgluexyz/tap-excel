baed466d42df5d2897538ea413f66cad0c843f32
Jong <jjleejong@gmail.com>
2023-10-10 10:25:00 -0500

    first commit

---
+++ tap_excel/client.py
@@ -0,0 +1,164 @@
+"""Custom client handling, including ExcelStream base class."""
+import os
+from datetime import datetime, timezone
+from typing import Iterable, List, Optional
+import pandas as pd
+
+from typing import Iterable
+
+from singer_sdk.streams import Stream
+from singer_sdk import typing as th
+
+
+SDC_SOURCE_FILE_COLUMN = "_sdc_source_file"
+SDC_SOURCE_LINENO_COLUMN = "_sdc_source_lineno"
+SDC_SOURCE_FILE_MTIME_COLUMN = "_sdc_source_file_mtime"
+
+
+
+class ExcelStream(Stream):
+    """Stream class for Excel streams."""
+
+    file_paths: List[str] = []
+    header: List[str] = []
+
+    def __init__(self, *args, **kwargs):
+        """Init ExcelStream."""
+        # cache file_config so we dont need to go iterating the config list again later
+        self.file_config = kwargs.pop("file_config")
+        super().__init__(*args, **kwargs)
+
+    def get_records(
+        self,
+        context: dict | None,  # noqa: ARG002
+    ) -> Iterable[dict]:
+        """Return a generator of record-type dictionary objects.
+
+        The optional `context` argument is used to identify a specific slice of the
+        stream if partitioning is required for the stream. Most implementations do not
+        require partitioning and should ignore the `context` argument.
+        """
+
+        for file_path in self.get_file_paths():
+            file_last_modified = datetime.fromtimestamp(
+                os.path.getmtime(file_path), timezone.utc
+            )
+
+            file_lineno = -1
+
+            for row in self.get_rows(file_path):
+                file_lineno += 1
+
+                if not file_lineno:
+                    continue
+
+                if self.config.get("add_metadata_columns", False):
+                    row = [file_path, file_last_modified, file_lineno] + row
+
+                yield dict(zip(self.header, row))
+
+    def _get_recursive_file_paths(self, file_path: str) -> list:
+        file_paths = []
+
+        for dirpath, _, filenames in os.walk(file_path):
+            for filename in filenames:
+                file_path = os.path.join(dirpath, filename)
+                if self.is_valid_filename(file_path):
+                    file_paths.append(file_path)
+
+        return file_paths
+
+    def get_file_paths(self) -> list:
+        """Return a list of file paths to read.
+
+        This tap accepts file names and directories so it will detect
+        directories and iterate files inside.
+        """
+        # Cache file paths so we dont have to iterate multiple times
+        if self.file_paths:
+            return self.file_paths
+
+        file_path = self.file_config["path"]
+        if not os.path.exists(file_path):
+            raise Exception(f"File path does not exist {file_path}")
+
+        file_paths = []
+        if os.path.isdir(file_path):
+            clean_file_path = os.path.normpath(file_path) + os.sep
+            file_paths = self._get_recursive_file_paths(clean_file_path)
+        else:
+            if self.is_valid_filename(file_path):
+                file_paths.append(file_path)
+
+        if not file_paths:
+            raise Exception(
+                f"Stream '{self.name}' has no acceptable files. \
+                    See warning for more detail."
+            )
+        self.file_paths = file_paths
+        return file_paths
+
+    def is_valid_filename(self, file_path: str) -> bool:
+        """Return a boolean of whether the file includes XLSX extension."""
+        valid_extentions = ["xls", "xlsx", "xlsm", "xlsb", "odf", "ods", "odt"]
+        is_valid = True
+        if file_path[-4:] not in valid_extentions:
+            is_valid = False
+            self.logger.warning(f"Skipping non-excel file '{file_path}'")
+            self.logger.warning(
+                "Please provide a excel file; e.g. 'users.xlsx'"
+            )
+
+        return is_valid
+
+    def get_rows(self, file_path: str) -> Iterable[list]:
+        """Return a generator of the rows in a particular xlsx file."""
+        sheet = 0
+        if self.file_config["sheet_name"] != "0":
+            sheet = self.file_config["sheet_name"]
+
+        excel_file = pd.read_excel(file_path, index_col=0, header=None, sheet_name = sheet)
+        
+        for row in excel_file.values.tolist():
+            yield row
+
+    @property
+    def schema(self) -> dict:
+        """Return dictionary of record schema.
+
+        Dynamically detect the json schema for the stream.
+        This is evaluated prior to any records being retrieved.
+        """
+        properties: List[th.Property] = []
+        self.primary_keys = self.file_config.get("keys", [])
+
+        for file_path in self.get_file_paths():
+            for header in self.get_rows(file_path):
+                break
+            break
+            
+
+        #replace with df
+        for column in header:
+            # Set all types to string
+            # TODO: Try to be smarter about inferring types.
+            properties.append(th.Property(column, th.StringType()))
+
+        # If enabled, add file's metadata to output
+        if self.config.get("add_metadata_columns", False):
+            header = [
+                SDC_SOURCE_FILE_COLUMN,
+                SDC_SOURCE_FILE_MTIME_COLUMN,
+                SDC_SOURCE_LINENO_COLUMN,
+            ] + header
+
+            properties.append(th.Property(SDC_SOURCE_FILE_COLUMN, th.StringType))
+            properties.append(
+                th.Property(SDC_SOURCE_FILE_MTIME_COLUMN, th.DateTimeType)
+            )
+            properties.append(th.Property(SDC_SOURCE_LINENO_COLUMN, th.IntegerType))
+
+        # Cache header for future use
+        self.header = header
+
+        return th.PropertiesList(*properties).to_dict()
---
+++ tap_excel/tap.py
@@ -0,0 +1,91 @@
+"""Excel tap class."""
+
+import json
+import os
+from typing import List
+
+from singer_sdk import Stream, Tap
+from singer_sdk import typing as th  # JSON schema typing helpers
+from singer_sdk.helpers._classproperty import classproperty
+from singer_sdk.helpers.capabilities import TapCapabilities
+
+from tap_excel.client import ExcelStream
+
+
+class TapExcel(Tap):
+    """Excel tap class."""
+
+    name = "tap-excel"
+
+    config_jsonschema = th.PropertiesList(
+        th.Property(
+            "files",
+            th.ArrayType(
+                th.ObjectType(
+                    th.Property("entity", th.StringType, required=True),
+                    th.Property("path", th.StringType, required=True),
+                    th.Property("keys", th.ArrayType(th.StringType), required=True),
+                    th.Property("sheet_name", th.StringType, required=False, default = "0")
+                )
+            ),
+            description="An array of excel file stream settings.",
+        ),
+        th.Property(
+            "excel_files_definition",
+            th.StringType,
+            description="A path to the JSON file holding an array of file settings.",
+        ),
+        th.Property(
+            "add_metadata_columns",
+            th.BooleanType,
+            required=False,
+            default=False,
+            description=(
+                "When True, add the metadata columns (`_sdc_source_file`, "
+                "`_sdc_source_file_mtime`, `_sdc_source_lineno`) to output."
+            ),
+        ),
+    ).to_dict()
+
+    @classproperty
+    def capabilities(self) -> List[TapCapabilities]:
+        """Get tap capabilites."""
+        return [
+            TapCapabilities.CATALOG,
+            TapCapabilities.DISCOVER,
+        ]
+
+    def get_file_configs(self) -> List[dict]:
+        """Return a list of file configs.
+
+        Either directly from the config.json or in an external file
+        defined by csv_files_definition.
+        """
+        excel_files = self.config.get("files")
+        excel_files_definition = self.config.get("excel_files_definition")
+        if excel_files_definition:
+            if os.path.isfile(excel_files_definition):
+                with open(excel_files_definition, "r") as f:
+                    excel_files = json.load(f)
+            else:
+                self.logger.error(f"tap-excel: '{excel_files_definition}' file not found")
+                exit(1)
+        if not excel_files:
+            self.logger.error("No excel file definitions found.")
+            exit(1)
+        return excel_files
+
+    def discover_streams(self) -> List[Stream]:
+        """Return a list of discovered streams."""
+        return [
+            ExcelStream(
+                tap=self,
+                name=file_config.get("entity"),
+                file_config=file_config,
+            )
+            for file_config in self.get_file_configs()
+        ]
+
+
+if __name__ == "__main__":
+    TapExcel.cli()
